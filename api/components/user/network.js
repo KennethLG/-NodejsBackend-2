d3e3b69c1ea3f5ccc8f08a4f99e4beb8e21040eb
KennethLG <orsonleiziaga@gmail.com>
2022-05-11 22:46:35 -0500

    dummy database

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const response = require("../../../network/response");
+const controller = require("./controller");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  response.success(req, res, "Todo correcto");
-})
+router.get("/", (req, res) => {
+  const list = controller.list();
+  response.success(req, res, list);
+});
 
 module.exports = router;
